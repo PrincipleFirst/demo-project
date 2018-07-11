ba8f93c489c899a0d5f89eaa0f049a5562e1b8c5
PrincipleFirst <364753362@qq.com>
2018-07-11 16:43:49 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,9 @@ module.exports = {
     '@vue/airbnb'
   ],
   rules: {
+    'semi': 0,
+    'indent': 0,
+    'max-len': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
